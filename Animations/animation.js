475fd14033567dd41c2c44e398220eddb6dd01ef
ronanquigley <info@ronanquigley.com>
2017-09-05 11:52:08 +0100

    Restructure of all svg elements and overal html document layout.

--- Animations/animation.js
+++ Animations/animation.js
@@ -1,22 +1,5 @@
-function Animate(element, data, xOffset, yOffset, width, height, bool, svgAspectRatio)
+function Animate(element, data, bool, svgAspectRatio)
 {
-
-  if(width === undefined)
-  {
-    width = 1920;
-  }
-  if(height === undefined)
-  {
-    height = 1080;
-  }
-  if(xOffset === undefined)
-  {
-    xOffset = 0;
-  }
-  if(yOffset === undefined)
-  {
-    yOffset = 0;
-  }
 
   if(svgAspectRatio === undefined)
   {
@@ -24,6 +7,7 @@ function Animate(element, data, xOffset, yOffset, width, height, bool, svgAspect
   }
 
   var elem = document.getElementById(element);
+
   if(elem == null)
   {
     throw Error("Cannot find element to animate: " + element.toString());
@@ -44,14 +28,6 @@ function Animate(element, data, xOffset, yOffset, width, height, bool, svgAspect
 
   var anim;
 	anim = bodymovin.loadAnimation(animData);
-
-  /*anim.addEventListener('DOMLoaded', function()
-  {
-    var svg = elem.querySelector('.bodymovin svg');
-    var args = [xOffset, yOffset, width, height];
-    svg.setAttribute('viewBox', args.join(' ').toString());
-    svg.setAttribute('height', '' + height);
-  });*/
 }
 
 function CheckScrollPosition(element, data)
@@ -66,11 +42,44 @@ function CheckScrollPosition(element, data)
   })
 }
 
+
+function setSvgViewBox(svgID, xOffset, yOffset, width, height)
+{
+    $(document).ready(function()
+    {
+      var svg = document.getElementById(svgID); // get the svg we are working with
+      if(svg.length != 0) // if a div with this id exists...
+      {
+        var viewBox = xOffset + " " + yOffset + " " + width + " " + height;
+        svg.setAttribute("viewBox", viewBox); // apply our viewbox
+      }
+      else
+      {
+        console.error("Cannot find element with id: " + svg.id);
+      }
+    })
+}
+
+function setSvgAspectRatio(id, aspectRatio)
+{
+  var svg = document.getElementById(id); // get the relevant svg element
+  if(svg.length != 0) // if an svg with this id exists...
+  {
+    var att = document.createAttributeNS(null, "preserveAspectRatio");
+    att.value = aspectRatio;
+    svg.setAttributeNode(att); // set preserveAspectRatio onto our svg
+  }
+  else
+  {
+    console.error("Cannot find element with id: " + svg.id);
+  }
+}
+
 function Init()
 {
   // animate the first elements on load
-  Animate('planet', 'Animations/planet.json', 0, 300, 1920, 500, true, "xMidYMin meet");
-  Animate('skill-anim-web-dev', 'Animations/skills-web-development.json');
+  /*Animate('planet', 'Animations/planet.json', 0, 300, 1920, 500, true, "xMidYMin meet");
+  Animate('skill-anim-web-dev', 'Animations/skills-web-development.json', 0, 0, 1146, 670, true, "xMidYMid meet");
   Animate('skill-anim-html', 'Animations/skills-html.json');
   Animate('skill-anim-css', 'Animations/skills-css.json');
   Animate('skill-anim-jquery', 'Animations/skills-jquery.json');
@@ -83,7 +92,43 @@ function Init()
   Animate('anim-city-flyover-one', 'Animations/city-flyover-one.json', 0, 0, 700, 300, true, "xMidYMax meet");
   Animate('anim-city-flyover-three', 'Animations/city-flyover-three.json', 0, 0, 700, 300, true, "xMidYMax meet");
   Animate('anim-city-flyover-five', 'Animations/city-flyover-five.json', 0, 0, 700, 300, true, "xMidYMax meet");
+  */
 
+/*
+  Animate('skill-anim-web-dev', 'Animations/skills-web-development.json', true, "xMidYMax meet");
+  Animate('skill-anim-html', 'Animations/skills-html.json', true, "xMidYMax meet");
+  Animate('skill-anim-css', 'Animations/skills-css.json', true, "xMidYMax meet");
+  Animate('skill-anim-jquery', 'Animations/skills-jquery.json', true, "xMidYMax meet");
+  Animate('skill-anim-js', 'Animations/skills-javascript.json', true, "xMidYMax meet");
+  Animate('skill-anim-animation', 'Animations/skills-animation.json', true, "xMidYMax meet");
+*/
+
+
+    Animate('planet', 'Animations/planet.json', true, "xMidYMin meet");
+    Animate('skill-anim-web-dev', 'Animations/skills-web-development.json', true, "xMidYMid meet");
+    Animate('skill-anim-html', 'Animations/skills-html.json', true, "xMidYMid meet");
+    Animate('skill-anim-css', 'Animations/skills-css.json', true, "xMidYMid meet");
+    Animate('skill-anim-jquery', 'Animations/skills-jquery.json', true, "xMidYMid meet");
+    Animate('skill-anim-js', 'Animations/skills-javascript.json', true, "xMidYMid meet");
+    Animate('skill-anim-animation', 'Animations/skills-animation.json', true, "xMidYMid meet");
+
+  $(document).ready(function()
+  {
+
+    setSvgViewBox('skill-anim-web-dev', -500, -385, 2292, 1340);
+    setSvgViewBox('skill-anim-html', -840, -220, 2292, 1340);
+    setSvgViewBox('skill-anim-css', -450, -130, 2322, 1600);
+    setSvgViewBox('skill-anim-jquery', -850, -320, 2292, 1340);
+    setSvgViewBox('skill-anim-js', -860, -120, 2292, 1340);
+    setSvgViewBox('skill-anim-animation', -510, -250, 2292, 1340);
+
+    setSvgAspectRatio('blimp-web', "xMidYMid meet");
+    setSvgAspectRatio('blimp-html', "xMidYMid meet");
+    setSvgAspectRatio('blimp-css', "xMidYMid meet");
+    setSvgAspectRatio('blimp-anim', "xMidYMid meet");
+    setSvgAspectRatio('blimp-jquery', "xMidYMid meet");
+    setSvgAspectRatio('blimp-js', "xMidYMid meet");
+  })
 
   // check the rest of the elements are on screen
   // CheckScrollPosition('skills-bm-animation', 'Animations/skills.json');
